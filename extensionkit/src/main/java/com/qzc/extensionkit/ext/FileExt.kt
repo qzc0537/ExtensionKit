1dcb98b53e120b78aaebe850730a89e2740fb71e
qzc0537 <522343535@qq.com>
2020-03-22 14:00:20 +0800

    FileExt增加获取文件路径方法

--- extensionkit/src/main/java/com/qzc/extensionkit/ext/FileExt.kt
+++ extensionkit/src/main/java/com/qzc/extensionkit/ext/FileExt.kt
@@ -1,12 +1,17 @@
 package com.qzc.extensionkit.ext
 
+import android.content.ContentUris
 import android.content.Context
 import android.content.res.AssetFileDescriptor
+import android.database.Cursor
 import android.net.Uri
 import android.os.Build
 import android.os.Environment
+import android.provider.DocumentsContract
+import android.provider.MediaStore
 import androidx.core.content.FileProvider
 import android.text.TextUtils
+import androidx.annotation.RequiresApi
 import com.qzc.extensionkit.utils.getMimeType
 import java.io.*
 import java.nio.ByteBuffer
@@ -56,7 +61,7 @@ fun Context.getPictureDir(): File? {
 /**
  * 删除文件
  */
-fun Context.delFile(path: String): Boolean {
+fun Context.deleteFile(path: String): Boolean {
     if (TextUtils.isEmpty(path)) {
         return false
     }
@@ -325,7 +330,7 @@ fun Context.makeDir(parent: String, child: String): File? {
 }
 
 
-fun Context.isAndroidQFileExists( path: String): Boolean {
+fun Context.isAndroidQFileExists(path: String): Boolean {
     var afd: AssetFileDescriptor? = null
     val cr = this.contentResolver
     try {
@@ -340,4 +345,236 @@ fun Context.isAndroidQFileExists( path: String): Boolean {
         afd?.close()
     }
     return true
+}
+
+/**
+ * 把内容写入文件
+ *
+ * @param content
+ */
+fun saveTxtToSD(context: Context, content: String) {
+    //生成文件夹之后，再生成文件，不然会出错
+    val fileName = "log.txt"
+
+    val parentFile = File(context.externalCacheDir.absolutePath + "/" + "Log")
+    if (!parentFile.exists()) {
+        parentFile.mkdir()
+    }
+    val file = File(parentFile, fileName)
+    if (!file.exists()) {
+        try {
+            file.createNewFile()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+
+    }
+
+    var out: BufferedWriter? = null
+    try {
+        out = BufferedWriter(
+            OutputStreamWriter(
+                FileOutputStream(file, true), "gb2312"
+            )
+        )
+        out.write(content)
+        out.write("\r\n")
+    } catch (e: Exception) {
+        e.printStackTrace()
+    } finally {
+        try {
+            out!!.close()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+    }
+}
+
+/**
+ * Get a file path from a Uri. This will get the the path for Storage Access
+ * Framework Documents, as well as the _data field for the MediaStore and
+ * other file-based ContentProviders.<br></br>
+ * <br></br>
+ * Callers should check whether the path is local before assuming it
+ * represents a local file.
+ *
+ * @param context The context.
+ * @param uri     The Uri to query.
+ */
+@RequiresApi(Build.VERSION_CODES.KITKAT)
+fun getPath(context: Context?, uri: Uri?): String? {
+    var path: String? = null
+    if (null == context || uri == null)
+        return null
+
+    val isKitKat = Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT
+    // DocumentUri
+    if (isKitKat && DocumentsContract.isDocumentUri(context, uri)) {
+        // ExternalStorageDocumentsUri
+        if (isExternalStorageDocumentsUri(uri)) {
+            val docId = DocumentsContract.getDocumentId(uri)
+            val splits =
+                docId.split(":".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
+            val type = splits[0]
+            if ("primary".equals(type, ignoreCase = true)) {
+                path =
+                    Environment.getExternalStorageDirectory().toString() + File.separator + splits[1]
+            }
+        } else if (isDownloadsDocumentsUri(uri)) {
+            val docId = DocumentsContract.getDocumentId(uri)
+            val contentUri = ContentUris.withAppendedId(
+                Uri.parse("content://downloads/public_downloads"),
+                java.lang.Long.valueOf(docId)
+            )
+            path = getDataColumn(context, contentUri, null, null)
+        } else if (isMediaDocumentsUri(uri)) {
+            val docId = DocumentsContract.getDocumentId(uri)
+            val split = docId.split(":".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
+            val type = split[0]
+            var contentUri: Uri? = null
+            if ("image" == type) {
+                contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+            } else if ("video" == type) {
+                contentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI
+            } else if ("audio" == type) {
+                contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI
+            }
+            val selection = "_id=?"
+            val selectionArgs = arrayOf(split[1])
+            path = getDataColumn(context, contentUri, selection, selectionArgs)
+        }// MediaDocumentsUri
+        // DownloadsDocumentsUri
+    } else if ("content".equals(uri.scheme, true)) {
+        if (isGooglePhotosContentUri(uri))
+            return uri.lastPathSegment
+        path = getDataColumn(context, uri, null, null)
+    } else if ("file".equals(uri.scheme, true)) {
+        path = uri.path
+    }// File
+    // MediaStore (general)
+    return path ?: getFilePathByCopyFile(context, uri)
+}
+
+/**
+ * Get the value of the data column for this Uri. This is useful for
+ * MediaStore Uris, and other file-based ContentProviders.
+ *
+ * @param context       The context.
+ * @param uri           The Uri to query.
+ * @param selection     (Optional) Filter used in the query.
+ * @param selectionArgs (Optional) Selection arguments used in the query.
+ * @return The value of the _data column, which is typically a file path.
+ * @author paulburke
+ */
+fun getDataColumn(
+    context: Context, uri: Uri?, selection: String?,
+    selectionArgs: Array<String>?
+): String? {
+
+    var cursor: Cursor? = null
+    val column = "_data"
+    val projection = arrayOf(column)
+
+    try {
+        cursor =
+            context.contentResolver.query(uri!!, projection, selection, selectionArgs, null)
+        if (cursor != null && cursor.moveToFirst()) {
+            val column_index = cursor.getColumnIndexOrThrow(column)
+            return cursor.getString(column_index)
+        }
+    } catch (e: Exception) {
+        e.printStackTrace()
+    } finally {
+        cursor?.close()
+    }
+    return null
+}
+
+fun isExternalStorageDocumentsUri(uri: Uri): Boolean {
+    return "com.android.externalstorage.documents" == uri.authority
+}
+
+fun isDownloadsDocumentsUri(uri: Uri): Boolean {
+    return "com.android.providers.downloads.documents" == uri.authority
+}
+
+fun isMediaDocumentsUri(uri: Uri): Boolean {
+    return "com.android.providers.media.documents" == uri.authority
+}
+
+fun isGooglePhotosContentUri(uri: Uri): Boolean {
+    return "com.google.android.apps.photos.content" == uri.authority
+}
+
+fun getFilePathByCopyFile(context: Context, contentUri: Uri): String? {
+    val rootDataDir = context.filesDir
+    val fileName = getFileName(contentUri)
+    if (!TextUtils.isEmpty(fileName)) {
+        val copyFile = File(rootDataDir.toString() + File.separator + fileName)
+        copyFile(context, contentUri, copyFile)
+        return copyFile.absolutePath
+    }
+    return null
+}
+
+fun getFileName(uri: Uri?): String? {
+    if (uri == null) return null
+    var fileName: String? = null
+    val path = uri.path ?: return fileName
+    val count = path.lastIndexOf('/')
+    if (count != -1) {
+        fileName = path.substring(count + 1)
+    }
+    return fileName
+}
+
+fun copyFile(context: Context, srcUri: Uri, dstFile: File) {
+    try {
+        val inputStream = context.contentResolver.openInputStream(srcUri) ?: return
+        val outputStream = FileOutputStream(dstFile)
+        copyStream(inputStream, outputStream)
+        inputStream.close()
+        outputStream.close()
+    } catch (e: Exception) {
+        e.printStackTrace()
+    }
+
+}
+
+@Throws(Exception::class, IOException::class)
+fun copyStream(input: InputStream, output: OutputStream): Int {
+    val BUFFER_SIZE = 1024 * 2
+    val buffer = ByteArray(BUFFER_SIZE)
+    val inputStream = BufferedInputStream(input, BUFFER_SIZE)
+    val outputStream = BufferedOutputStream(output, BUFFER_SIZE)
+    var count = 0
+    var readCount = -1
+    try {
+        do {
+            readCount = inputStream.read(buffer, 0, BUFFER_SIZE)
+            if (readCount != -1) {
+                outputStream.write(buffer, 0, readCount)
+                count += readCount
+            } else {
+                break
+            }
+        } while (true)
+        outputStream.flush()
+    } catch (e: IOException) {
+        e.printStackTrace()
+    } finally {
+        try {
+            outputStream.close()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+
+        try {
+            inputStream.close()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+
+    }
+    return count
 }
